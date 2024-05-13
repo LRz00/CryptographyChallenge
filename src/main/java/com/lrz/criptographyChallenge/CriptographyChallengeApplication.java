838ed9e098a777e20b15a2da1b740da1ad03e935
Lara Rodrigues <lararods73@gmail.com>
2024-05-13 17:18:34 -0300

    initial commit

---
+++ src/main/java/com/lrz/criptographyChallenge/CriptographyChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.lrz.criptographyChallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CriptographyChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CriptographyChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lrz/criptographyChallenge/CriptographyChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lrz.criptographyChallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CriptographyChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
